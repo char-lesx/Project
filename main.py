7815a4876d2c6601c182e7dc7ac164ab78c1b259
char-lesx <charliewcutt@gmail.com>
2023-09-05 10:22:59 +0100

    IT WORKED!!!!!!!!!

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+import streamlit as st
+
 def main():
-    print("test")
+    st.title("Text Input Web App")
+    
+    # Text input
+    user_input = st.text_input("Enter some text:", "")
+    
+    # Display the input
+    st.write("You entered:", user_input)
 
 if __name__ == "__main__":
     main()
