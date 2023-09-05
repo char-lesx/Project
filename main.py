10d6f2f39a2e968f07840bac83d4efedd76a51e7
char-lesx <charliewcutt@gmail.com>
2023-09-05 10:12:48 +0100

    Didn't work, going back to Flask.

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
-import streamlit as st
-
 def main():
-    st.title("Text Input Web App")
-    
-    # Text input
-    user_input = st.text_input("Enter some text:", "")
-    
-    # Display the input
-    st.write("You entered:", user_input)
+    print("test")
 
 if __name__ == "__main__":
     main()
